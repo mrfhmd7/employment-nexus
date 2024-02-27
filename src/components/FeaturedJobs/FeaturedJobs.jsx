702f91e878ba9b1efcb4e7e92ea54449769aa2ed
mrfhmd7 <mrf.hmd7@gmail.com>
2024-02-27 23:22:59 +0600

    show logo, name, salary, location and others

--- src/components/FeaturedJobs/FeaturedJobs.jsx
+++ src/components/FeaturedJobs/FeaturedJobs.jsx
@@ -1,10 +1,27 @@
 import React from 'react';
 
-const FeaturedJobs = ({logo}) => {
+const FeaturedJobs = ({ logo, job_title, company_name, remote_or_onsite, job_type, location, salary }) => {
      return (
-          <div>
-               <h1>Featured jobs are here</h1>
-               <img src={logo} alt="" />
+          <div className='rounded-md border border-[#E8E8E8]  shadow-xl text-start'>
+               <div className='mt-8 ml-10'>
+                    <img src={logo} alt="" />
+                    <h3 className='text-2xl font-bold mt-4'>{job_title}</h3>
+                    <h4 className='text-xl font-semibold mt-3 text-[#757575]'>{company_name}</h4>
+                    <div className='inline-flex text-center mt-3'>
+                         <p className='p-2 border-2 rounded-md border-[#7E90FE] text-transparent bg-clip-text bg-gradient-to-r from-[#7E90FE] to-[#9873FF]'>{remote_or_onsite}</p>
+                         <p className='p-2 border-2 rounded-md border-[#7E90FE] ml-4 text-transparent bg-clip-text bg-gradient-to-r from-[#7E90FE] to-[#9873FF]'>{job_type}</p>
+                    </div>
+                    <div className='flex gap-4 mt-5 text-[#757575]'>
+                         <p className='flex'>
+                              <img src='/icons/Location.png' alt="" />
+                              {location}
+                         </p>
+                         <p className='flex'>
+                              <img src="/public/icons/money.png" alt="" />
+                              {salary}
+                         </p>
+                    </div>
+               </div>
           </div>
      );
 };
--- src/components/Jobs/Jobs.jsx
+++ src/components/Jobs/Jobs.jsx
@@ -5,7 +5,7 @@ import FeaturedJobs from '../FeaturedJobs/FeaturedJobs';
 const Jobs = () => {
      const [jobs, setJobs] = useState([]);
      const [categories, setCategories] = useState([]);
-     // console.log(jobs);
+     console.log(jobs);
      // console.log(categories);
 
      useEffect(() => {
@@ -25,7 +25,7 @@ const Jobs = () => {
      return (
           <div>
                <div>
-                    <h2 className='font-bold mt-20 text-5xl'>Jobs Category List</h2>
+                    <h2 className='font-bold mt-24 text-5xl'>Jobs Category List</h2>
                     <p className='text-[#757575] mt-6'>Explore thousands of job opportunities with all the information you need. Its your future</p>
                     <div className='flex justify-between mt-8'>
                          {
@@ -39,13 +39,19 @@ const Jobs = () => {
                     </div>
                </div>
                <div>
-                    <h2>Featured Jobs</h2>
-                    <p>Explore thousands of job opportunities with all the information you need. Its your future</p>
-                    <div>
+                    <h2 className='text-5xl font-bold text-[#1A1919] mt-24'>Featured Jobs</h2>
+                    <p className='mt-5 mb-6 text-[#757575]'>Explore thousands of job opportunities with all the information you need. Its your future</p>
+                    <div className='grid grid-cols-2 gap-10'>
                          {
                               jobs.map(job => <FeaturedJobs
                                    key={job.id}
                                    logo={job.logo}
+                                   job_title={job.job_title}
+                                   company_name={job.company_name}
+                                   remote_or_onsite={job.remote_or_onsite}
+                                   job_type={job.job_type}
+                                   location={job.location}
+                                   salary={job.salary}
                               ></FeaturedJobs>)
                          }
                     </div>
