ae62cd6f4fca1bd6be39e3872e5ba88916de3ce4
mrfhmd7 <mrf.hmd7@gmail.com>
2024-02-29 21:58:48 +0600

    rechart added

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -1,10 +1,62 @@
 import React from 'react';
+import { AreaChart, Area, XAxis, YAxis, CartesianGrid, Tooltip, ResponsiveContainer } from 'recharts';
+
+const data =[
+     {
+         name: "assignment1",
+         mark: 55
+     },
+     {
+         name: "assignment2",
+         mark: 50
+     },
+     {
+         name: "assignment3",
+         mark: 52
+     },
+     {
+         name: "assignment4",
+         mark: 57
+     },
+     {
+         name: "assignment5",
+         mark: 54
+     },
+     {
+         name: "assignment6",
+         mark: 60
+     },
+     {
+         name: "assignment7",
+         mark: 45
+     },
+     {
+         name: "assignment8",
+         mark: 58
+     },
+     {
+          name: "assignment9",
+          mark: 53
+      },
+     {
+         name: "assignment10",
+         mark: 50
+     }
+ ]
+ 
 
 const Statistics = () => {
+
      return (
-          <div>
-               <h3>Rechart is coming soon..</h3>
-          </div>
+          <ResponsiveContainer width="100%" height={400}>
+               <AreaChart data={data}>
+                    <CartesianGrid strokeDasharray="3 3" />
+                    <XAxis dataKey="name" />
+                    <YAxis />
+                    <Tooltip />
+                    <Area type="monotone" dataKey="mark" stroke="#8884d8" fill="#8884d8" />
+               </AreaChart>
+          </ResponsiveContainer>
      );
 };
 
