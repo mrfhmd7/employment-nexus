a41d884d8bd864511667eaca2d943eedda20a59f
mrfhmd7 <mrf.hmd7@gmail.com>
2024-03-03 23:35:40 +0600

    finished

--- src/components/Landing/Landing.jsx
+++ src/components/Landing/Landing.jsx
@@ -2,17 +2,19 @@ import React from 'react';
 
 const Landing = () => {
      return (
-          <div className='flex bg-[#9873FF0D]'>
-               <div className=' text-start'>
-                    <h2 className='font-extrabold text-6xl flex items-center mt-32 leading-snug'>One Step
-                         <br />Closer To Your
+          <div className='flex flex-col-reverse md:flex-row bg-[#9873FF0D]'>
+               <div className='text-center md:text-left px-4 md:px-8 py-12 md:py-32 md:w-1/2'>
+                    <h2 className='font-extrabold text-4xl md:text-6xl leading-tight md:leading-snug'>
+                         One Step Closer To Your
+                         <br />
+                         <span className='text-transparent bg-clip-text bg-gradient-to-r from-[#7E90FE] to-[#9873FF]'>Dream Job</span>
                     </h2>
-                    <span className='font-extrabold text-start text-6xl text-transparent bg-clip-text bg-gradient-to-r from-[#7E90FE] to-[#9873FF]'> Dream Job</span>
-                    <p className='pt-12 pb-6 text-[#757575] w-9/12'>Explore thousands of job opportunities with all the information you need. Its your future. Come find it. Manage all your job application from start to finish.</p>
+                    <p className='py-6 text-[#757575]'>Explore thousands of job opportunities with all the information you need. It's your future. Come find it. Manage all your job applications from start to finish.</p>
                     <button className='bg-gradient-to-l from-[#7E90FE] to-[#9873FF] rounded-lg h-12 w-32 text-white text-lg font-bold'>Get Started</button>
                </div>
-               <img className='h-[600px] w-auto' src="/images/user.png" alt="" />
+               <img className='h-auto md:h-[600px] w-full md:w-auto' src="/images/user.png" alt="" />
           </div>
      );
 };
+
 export default Landing;
